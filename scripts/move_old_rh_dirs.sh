f016a20c800d1d581fbd031b88e38e67684bdbf5
szafranski <pawel.fabiszewski@trecom.pl>
2023-12-30 23:12:44 +0100

    handling existing installations

--- scripts/move_old_rh_dirs.sh
+++ scripts/move_old_rh_dirs.sh
@@ -10,7 +10,4 @@ if [ -d "/home/${1}/RotorHazard-${2}" ]; then
   mv "/home/${1}/RotorHazard-${2}" "/home/${1}/RotorHazard_${2}_$(date +%Y%m%d%H%M)" || exit 1
 fi
 
-if [ -d "/home/${1}/RotorHazard-*" ]; then
-  # Control will enter here if $DIRECTORY exists.
-  mv "/home/${1}/RotorHazard*" "/home/${1}/RotorHazard_${2}_$(date +%Y%m%d%H%M)" || exit 1
-fi
+  mv "/home/${1}/RotorHazard-*" "/home/${1}/RotorHazard_${2}_$(date +%Y%m%d%H%M)" > /dev/null 2>&1
