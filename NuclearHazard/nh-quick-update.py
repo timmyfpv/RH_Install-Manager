7330ef826a212b6e25eb247407a34e876042b0e5
szafranski <pawel.fabiszewski@trecom.pl>
2024-01-22 14:31:36 +0100

    nh update

---
+++ NuclearHazard/nh-quick-update.py
@@ -0,0 +1,24 @@
+from rpi_update import update
+import os
+import json
+from types import SimpleNamespace as Namespace, SimpleNamespace
+from compatibility_check import main as compatibility_check
+
+
+def load_json(file_name):
+    data = {}
+    if os.path.exists(file_name):
+        with open(file_name) as open_file:
+            data = json.loads(open_file.read(), object_hook=lambda d: Namespace(**d))
+    return data
+
+
+def main():
+    compatibility_check()
+    passed_install_step = os.getenv('INSTALL_STEP')
+    config = load_json("./nh-updater-config.json")
+    update(config, False)
+
+
+if __name__ == "__main__":
+    main()
