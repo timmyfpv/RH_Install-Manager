6f553ee4571e374ee51c2ed066bd009cd55b346e
szafranski <pawel.fabiszewski@trecom.pl>
2024-01-25 16:09:17 +0100

    config check

--- NuclearHazard/nh-quick-install.py
+++ NuclearHazard/nh-quick-install.py
@@ -10,9 +10,10 @@ from compatibility_check import main as compatibility_check
 def name_check():
     username = pwd.getpwuid(os.getuid()).pw_name
     if username != "NuclearHazard":
-        print("\n\n\t\tThis script assumes your username is 'NuclearHazard'\n\n\n")
-        input("Press Enter to exit")
+        print("\n\n\t\tThis script assumes your username is 'NuclearHazard'")
+        input("\n\n\t\tPress Enter to exit")
         sys.exit()
+    return username
 
 
 def load_json(file_name):
@@ -24,10 +25,11 @@ def load_json(file_name):
 
 
 def main():
-    name_check()
+    username = name_check()
     compatibility_check()
     passed_install_step = os.getenv('INSTALL_STEP')
     config = load_json("./nh-updater-config.json")
+    config.name = username
     installation(True, config, False, passed_install_step)
 
 
