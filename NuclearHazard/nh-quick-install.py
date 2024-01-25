7a938ade08a709e99fbdda18fde930ceb36b79bc
szafranski <pawel.fabiszewski@trecom.pl>
2024-01-25 13:18:54 +0100

    NH echo added

--- NuclearHazard/nh-quick-install.py
+++ NuclearHazard/nh-quick-install.py
@@ -1,8 +1,18 @@
 from rpi_update import installation
 import os
+import sys
+import pwd
 import json
 from types import SimpleNamespace as Namespace, SimpleNamespace
 from compatibility_check import main as compatibility_check
+
+
+def name_check():
+    username = pwd.getpwuid(os.getuid()).pw_name
+    if username != "NuclearHazard":
+        print("\n\n\t\tThis script assumes your username is 'NuclearHazard\n\n")
+        input("Press Enter to exit")
+        sys.exit()
 
 
 def load_json(file_name):
@@ -14,6 +24,7 @@ def load_json(file_name):
 
 
 def main():
+    name_check()
     compatibility_check()
     passed_install_step = os.getenv('INSTALL_STEP')
     config = load_json("./nh-updater-config.json")
