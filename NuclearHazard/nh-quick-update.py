41d1b506117bbb14e3f81de63c3f414b4617bf58
szafranski <pawel.fabiszewski@trecom.pl>
2024-01-27 00:01:17 +0100

    added nh update selection

--- NuclearHazard/nh-quick-update.py
+++ NuclearHazard/nh-quick-update.py
@@ -1,17 +1,7 @@
 from rpi_update import update
 import os
-import json
 from modules import write_json, load_json
-from types import SimpleNamespace as Namespace, SimpleNamespace
 from compatibility_check import main as compatibility_check
-
-
-def load_json(file_name):
-    data = {}
-    if os.path.exists(file_name):
-        with open(file_name) as open_file:
-            data = json.loads(open_file.read(), object_hook=lambda d: Namespace(**d))
-    return data
 
 
 def main():
