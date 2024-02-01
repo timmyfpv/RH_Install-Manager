c7a77776a1a603b29c57c0a687fcaa5f9f581755
szafranski <pawel.fabiszewski@trecom.pl>
2024-02-01 21:37:59 +0100

    quick install fix

--- NuclearHazard/nh-install.sh
+++ NuclearHazard/nh-install.sh
@@ -2,10 +2,10 @@
 
 export PYTHONPATH=/home/"$USER"/RH_Install-Manager:$PYTHONPATH
 
-sh ../rhim.sh no_start
+sh ~/RH_Install-Manager/rhim.sh no_start
 if [ "$1" == "wifi" ]; then
-  sh ./nh-wifi.sh
+  sh ~/RH_Install-Manager/NuclearHazard/nh-wifi.sh
 else
   export INSTALL_STEP="$1"
-  python3 ./nh-quick-install.py
+  python3 ~/RH_Install-Manager/NuclearHazard/nh-quick-install.py
 fi
